215db490e915ef18cb1e5555083681cec403567f
lzt <bjliuzhantao@gmail.com>
2016-05-10 13:03:17 +0800

    Create redis_nodes.lua
    redis配制文件

---
+++ redis_nodes.lua
@@ -0,0 +1,17 @@
+local _M = {}
+_M.nodes = { 
+    read={
+        {addr="10.4.96.12",port=6379,timeout=3000},
+        {addr="10.4.96.13",port=6379,timeout=3000},
+        --{addr="10.4.18.129",port=6379,timeout=3000},
+        --{addr="unix:/path/to/unix.sock",port=nil,timeout=1000},
+    },  
+    write={
+        {addr="10.4.18.129",port=6379,timeout=3000},
+        --{addr="unix:/path/to/unix.sock",port=nil,timeout=1000},
+    }   
+}
+
+local mt = { __index = _M }
+
+return _M
