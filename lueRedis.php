07b8b541ec0c9773eaf10f6c0fee0dd281cddb12
lzt <bjliuzhantao@gmail.com>
2016-05-11 17:10:34 +0800

    Update lueRedis.php
    实现exists、zrevrangebyscore 返回结果解析：保持和redis扩展结果一致

--- lueRedis.php
+++ lueRedis.php
@@ -4,7 +4,6 @@
  * Copyright (c) 2016 ByteDance.com, Inc. All Rights Reserved
  *
  **************************************************************************/
-
 /**
  *@file LuaRedis.php
  *
@@ -17,20 +16,17 @@
  */
 /*
 demo : 
-    $test = lua_redis::getInstance();
-
+    $test = RedisProxy::getInstance();
     $ret = $test->set('k1','v1');
     $ret = $test->get('k1');
-
     $test->mset('k1','v1','k2','v2');
     $test->mget('k1','k2','k3','k4');
-
     //pipeline
     $test->pipeline('mset','k1','v1','k2','v2');
     $test->pipeline('mget','k1','k2','k3');
     $ret = $test->commit();
 */
-class LuaRedis
+class RedisProxy
 {
     private $url_ = 'http://10.4.18.124/rds';
     private $db_ = 1;
@@ -39,10 +35,7 @@ class LuaRedis
     private $srv_ = 'duoshuo';
     private $error_ = null;
     public $commands_ = [];
-
     static private $instance_ = null;
-
-
     public static function getOne()
     {
         return self::getInstance();
@@ -57,7 +50,6 @@ class LuaRedis
     {
         return $this->error_;
     }
-
     /**
      *@desc 普通方法调用
      */
@@ -66,10 +58,8 @@ class LuaRedis
         $command = $func; 
         if (!empty($params))
             $command .= '/'. implode('/',$params);
-
         $url = $this->url_ .'/' . $command . '?' . $this->genArgs(); 
         $result = $this->getReq($url);
-
         return $this->parseGetReq($func,$result);
     }
     /**
@@ -77,7 +67,7 @@ class LuaRedis
      */
     private function parseGetReq($command,$result)
     {
-        if (empty($result))
+        if (is_array($result) && empty($result))
         {
             return $result;
         }
@@ -95,15 +85,30 @@ class LuaRedis
      */
     private function parse_zrevrangebyscore($params)
     {
-         
+        $ret = []; 
+        foreach($params as $k=>$v)
+        {
+            if ($k % 2 == 0)
+            {
+                $ret[$v] = $params[$k+1];
+            }
+        }
+        return $ret;
+    }
+    private function parse_exists($p)
+    {
+        return $p === 1 ? true : false; 
     }
     /**
-     *@desc pipeline调用
+     *@desc pipeline调用,要和commit配合使用
      */
     public function pipeline()
     {
         $this->commands_[] = func_get_args(); 
     }
+    /**
+     *@desc pipeline调用,要和pipeline配合使用
+     */
     public function commit()
     {
         if (empty($this->commands_))
@@ -111,7 +116,6 @@ class LuaRedis
             $this->error_ .= '没有传递参数。';
             return false;
         }
-
         $data['pipeline'] = json_encode($this->commands_);
         $url = $this->url_ .'?' . $this->genArgs();
         return $this->post($url,$data);
@@ -124,11 +128,17 @@ class LuaRedis
         $ret = $this->http($url,'GET',NULL,$headers);
         return $ret;
     }
+    /**
+     *@desc 调用post方法
+     */
     public function post($url,$data,$headers = [])
     {
         $ret = $this->http($url,'POST',$data,$headers);
         return $ret;
     }
+    /**
+     *@desc 解析redis-proxy返回的reponse
+     */
     private function parseResponse($str)
     {
         $result = false;
@@ -151,6 +161,9 @@ class LuaRedis
         while(false);
         return $result;
     }
+    /**
+     *@desc 生成query string
+     */
     private function genArgs()
     {
         $uri = 'srv=' . (empty($this->srv_) ? 'default' : $this->srv_); 
@@ -164,6 +177,9 @@ class LuaRedis
             $uri .= '&debug=' . $this->debug_;
         return $uri;
     }
+    /**
+     *@desc http request
+     */
     public function http($url, $method = 'GET', $postfields = NULL, $headers = array()) 
     {
         $this->http_info = array();
@@ -173,7 +189,6 @@ class LuaRedis
         curl_setopt($ci, CURLOPT_CONNECTTIMEOUT, 1);
         curl_setopt($ci, CURLOPT_TIMEOUT, 3);
         curl_setopt($ci, CURLOPT_RETURNTRANSFER, TRUE);
-
         if ('POST' == $method)
         {
             curl_setopt($ci, CURLOPT_POST, TRUE);
